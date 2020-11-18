d73e6985d6a7596a87457ff08b8c25fa8970f5f4
nletcher <sofiamwaliza@gmail.com>
2020-11-18 12:52:50 -0800

    Flask app

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect
+import pymongo
+import scrape_mars
+
+# Use PyMongo to establish Mongo connection
+client = pymongo.MongoClient('mongodb://localhost:27017')
+db = client.mars_db
+collection = db.mars
+# Create an instance of Flask
+app = Flask(__name__)
+
+
+# Route to render index.html template using data from Mongo
+@app.route('/')
+def home():
+    mars_info = collection.find_one()
+    return render_template('index.html', mars_info=mars_info)
+
+@app.route('/scrape')
+def scrape():
+    # Run the scrape function and return data
+    #
+    scrape_mars.scrape()
+    return redirect('/', code = 302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
